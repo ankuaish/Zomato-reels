bb2cf799361614cedeed1d8ac8d0aa78a5755326
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-12-03 22:53:16 +0530

    create food

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
+const foodRoutes = require("./routes/food.routes");
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
 
 module.exports = app;

f45bf6f20e4347e3ca3146a52f9ee319b6a4314c
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-12-04 14:48:30 +0530

    image

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,6 +1,11 @@
 const foodModel = require("../models/food.model");
 
-async function createFood(req, res) {}
+async function createFood(req, res) {
+  res.send("food item created ");
+  console.log(req.foodPartner);
+  console.log(req.body);
+  console.log(req.file);
+}
 
 module.exports = {
   createFood,
